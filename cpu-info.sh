68b0500178049c2185ae811fe29e5a7f6a9ffde8
Pedro Sérgio <www.psro@gmail.com>
2021-02-08 09:40:53 -0300

    mem information graphic

--- cpu-info.sh
+++ cpu-info.sh
@@ -2,6 +2,7 @@
 
 # generate a random filename
 tmp_file=$(openssl rand -base64 32 | tr -dc A-Za-z0-9 && echo ".html")
+
 touch $tmp_file
 
 # open body
@@ -20,20 +21,23 @@ echo "<body style='
         font-family: monospace;
     '>" >> $tmp_file
 
-
-# script title
+# window title
 echo "<h1 align=center> Computer info </h1>" >> $tmp_file
 
 
 # catch cpu info
+
 ## section title
 echo "<h2> ⚙ CPU </h2>" >> $tmp_file
 
-
 cpu_info=$(openssl rand -base64 32 | tr -dc A-Za-z0-9 && echo ".txt")
+
 touch $cpu_info
+
 cat /proc/cpuinfo >> $cpu_info
+
 info_icon="<span style='color: gold;'> ★ </span>"
+
 declare -a information=(
     "model name"
     "cpu MHz"
@@ -47,13 +51,18 @@ for info in "${information[@]}"; do
 done
 
 # catch mem info
+
 ## section title
 echo "<h2> ⛁ Memory </h2>" >> $tmp_file
 
 mem_info=$(openssl rand -base64 32 | tr -dc A-Za-z0-9 && echo ".txt")
+
 touch $mem_info
+
 cat /proc/meminfo >> $mem_info
+
 info_icon="<span style='color: gold;'> ★ </span>"
+
 declare -a information=(
     "MemTotal"
     "MemFree"
@@ -65,6 +74,19 @@ for info in "${information[@]}"; do
     cat $mem_info | grep "$info" | head -1 >> $tmp_file
     echo "<br>" >> $tmp_file
 done
+
+mem_total=$(cat $mem_info | grep "MemTotal" | grep "[0-9]*" -o)
+mem_free=$(cat $mem_info | grep "MemFree" | grep "[0-9]*" -o)
+
+
+let mem_free_percentage="100 * $mem_free / $mem_total"
+let mem_total_percentage="100-$mem_free_percentage"
+
+echo "<br>" >> $tmp_file
+echo "<div style='display: flex'>" >> $tmp_file
+    echo "<div style='padding:1rem; width: $mem_free_percentage%; height: 40px; background: green'></div>" >> $tmp_file 
+    echo "<div style='padding:1rem; width: $mem_total_percentage%; height: 40px; background: red'></div>" >> $tmp_file
+echo "</div>" >> $tmp_file
 
 # close body
 echo "</body>" >> $tmp_file
